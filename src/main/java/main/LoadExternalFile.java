35009ef06aa4cf2a4e8b533232aa00d8c7ec28ff
angeldalberici <Angel.Trastoy@AddisonLee.com>
2017-09-26 16:44:06 +0100

    Code cleaning

--- src/main/java/main/LoadExternalFile.java
+++ src/main/java/main/LoadExternalFile.java
@@ -14,40 +14,12 @@ import com.github.fge.jsonschema.library.Library;
 
 
 public class LoadExternalFile implements Callable{
-	String body;
-	
 	@Override
 	public Object onCall(MuleEventContext eventContext) throws Exception {
-		System.out.println(eventContext.getMessage().getPayload().toString());
-		eventContext.getMessage().setInvocationProperty("myProperty", "Hello World!");
-        eventContext.getMessage().getPayload().toString();
-        System.out.println("-------------------------X123------------------------");
-		String fileName = "C://sav4//external-property//src//main//resources//local.properties";
-
-		String content = new String(Files.readAllBytes(Paths.get("C://sav4//external-property//src//main//resources//local.properties")));
-		body = content;
-        
 		ObjectMapper mapper = new ObjectMapper();
+		String content = new String(Files.readAllBytes(Paths.get("C://sav4//external-property//src//main//resources//local.properties")));
 		Map<String,Object> map = mapper.readValue(content, Map.class);
-		
         return map;
-        
     }
-	
-	public static void main(String args[]) {
-
-		String fileName = "C://sav4//cry//src//main//resources//local.properties";
-
-		//read file into stream, try-with-resources
-		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-
-			stream.forEach(System.out::println);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
 
 }
